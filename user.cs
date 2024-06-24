808fdb76956ea60e63db7fe123f4b6fbd6d618cd
Nika275 <nika.makharadze.3@iliauni.edu.ge>
2024-06-24 23:15:58 +0400

    Update user.cs

--- user.cs
+++ user.cs
@@ -1,1 +1,55 @@
+public class User {
+    private int userID;
+    private Username username;
+    private Password password;
+    private String email;
+    private Date createdAt;
 
+    public User(int userID, Username username, Password password, String email, Date createdAt) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public Username getUsername() {
+        return username;
+    }
+
+    public void setUsername(Username username) {
+        this.username = username;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
